e9bd5d518d99f6827179e40c1eec19104939555d
童永泽 <604232797@qq.com>
2021-02-04 15:03:20 +0800

    实现class的切换

--- src/views/classandstyle.vue
+++ src/views/classandstyle.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    <ul class="tab__ul--main">
-      <li>index1</li>
-      <li>index2</li>
-      <li>index3</li>
-      <li>index4</li>
-      <li>index5</li>
+    <button @click="flag = !flag">切换tab栏</button>
+    <ul class="tab__ul--main" v-if="flag">
+      <li
+      v-for="item in 6"
+      :key="item"
+      :class="{active:currActice === item }"
+      @click="toggle(item)">{{ item }}</li>
+    </ul>
+
+      <ul class="tab__ul--main" v-else>
+      <li
+      v-for="item in 13"
+      :key="item"
+      :class="{active:currActice === item }"
+      @click="toggle(item)">{{ item }}</li>
     </ul>
   </div>
 </template>
@@ -13,6 +22,18 @@
 <script>
 export default {
   name: 'ClassAndStyle',
+  data() {
+    return {
+      currActice: 1,
+      flag: true
+    };
+  },
+  methods: {
+    toggle(param) {
+      // console.log(param);
+      this.currActice = param
+    }
+  },
 };
 </script>
 
@@ -31,12 +52,20 @@ ul {
     text-align: center;
     padding: 5px 10px;
     border-right: none;
+    cursor: pointer;
     &:last-child {
-       border: 1px solid #ccc;
-        border-radius: 0 4px 4px 0;
+      border: 1px solid #ccc;
+      border-radius: 0 4px 4px 0;
     }
-      &:first-child {
-       border-radius: 4px 0 0 4px;
+    &:first-child {
+      border-radius: 4px 0 0 4px;
+    }
+    &:hover {
+      background-color: rgba(200, 200, 200, 0.3);
+    }
+    &.active {
+      color: white;
+        background-color: rgba(200, 200, 200, 1);
     }
   }
 }
